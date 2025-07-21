0b03f108fd9e330b9ba5a0f64ba2806b6a25693c
rui wu <141422141+arw131072@users.noreply.github.com>
2025-07-20 21:19:24 -0400

    pickup object & script, walls

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+ // Reference to the player GameObject.
     public GameObject player;
-    private Vector3 offset; // offset distance between player and camera
-
-    void Start()
+ // The distance between the camera and the player.
+    private Vector3 offset;
+ // Start is called before the first frame update.
+ void Start()
     {
-        offset = transform.position - player.transform.position; // initial offset
+ // Calculate the initial offset between the camera's position and the player's position.
+        offset = transform.position - player.transform.position; 
     }
 
-    // Update is called once per frame
-    void LateUpdate()
+ // LateUpdate is called once per frame after all Update functions have been completed.
+ void LateUpdate()
     {
-        transform.position = player.transform.position + offset; // new position
-
+ // Maintain the same offset between the camera and player throughout the game.
+        transform.position = player.transform.position + offset;  
     }
 }
